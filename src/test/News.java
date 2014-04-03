41c0f257c266743d271ed7017cc11bae66cace9d
dongxl <4392604@qq.com>
2014-04-03 14:34:11 +0800

    the first git hub.

---
+++ src/test/News.java
@@ -0,0 +1,47 @@
+/**
+ * Created by dongxl on 14-4-3.
+ */
+package test;
+
+/**
+ * Created by dongxl on 14-4-2.
+ */
+public class News {
+    //消息类的标识属性
+    private int id;
+    //消息标题
+    private String title;
+    //消息内容
+    private String content;
+
+    //构造器
+    public News() {
+    }
+
+    public int getId() {
+        return (this.id);
+    }
+
+    //标识属性的setter和getter方法
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return (this.title);
+    }
+
+    //消息标题的setter方法和getter方法
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return (this.content);
+    }
+
+    //消息内容的setter方法和getter方法
+    public void setContent(String content) {
+        this.content = content;
+    }
+}

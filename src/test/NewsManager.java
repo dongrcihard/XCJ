e290b6dcde19e9e25422fb6b79f8e0e97d5b7355
dongxl <4392604@qq.com>
2014-04-05 08:29:05 +0800

    The project works ok.

--- src/test/NewsManager.java
+++ src/test/NewsManager.java
@@ -4,14 +4,18 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
 
 public class NewsManager {
 
     public static void main(String[] args) throws Exception {
         //实例化Configuration，这行代码默认加载hibernate.cfg.xml文件
         Configuration conf = new Configuration().configure();
+        ServiceRegistry  sr = new ServiceRegistryBuilder().applySettings(conf.getProperties()).buildServiceRegistry();
+        SessionFactory sf = conf.buildSessionFactory(sr);
         //以Configuration创建SessionFactory
-        SessionFactory sf = conf.buildSessionFactory();
+//        SessionFactory sf = conf.buildSessionFactory();
         //实例化Session
         Session sess = sf.openSession();
         //开始事务
@@ -19,14 +23,14 @@ public class NewsManager {
         //创建消息实例
         News n = new News();
         //设置消息标题和消息内容
-        n.setTitle("疯狂Java联盟成立了FFFF");
-        n.setContent("疯狂Java联盟成立了，"
-                + "网站地址http://www.crazyjava.org");
+        n.setTitle("hibernate");
+        n.setContent("i am testing.");
         //保存消息
         sess.save(n);
         //提交事务
         tx.commit();
         //关闭Session
         sess.close();
+
     }
 }
